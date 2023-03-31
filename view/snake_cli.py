95ec13b50f44f59d3669191f59dd56e41b7e460d
RhamzThev <rhamzthev@gmail.com>
2023-03-31 19:08:58 -0400

    cli updates

--- view/snake_cli.py
+++ view/snake_cli.py
@@ -3,16 +3,29 @@ from snake_ui import UI
 
 class CLI(UI):
     def __init__(self, width, height) -> None:
-        pass
+        self.__stdscr  = curses.initscr()
 
     def get_event(self) -> str:
-        raise NotImplementedError("Subclass must implement this method")
-
+        event = self.__stdscr.getch()
+        if event == curses.KEY_UP:
+            return "UP"
+        elif event == curses.KEY_RIGHT:
+            return "RIGHT"
+        elif event == curses.KEY_DOWN:
+            return "DOWN"
+        elif event == curses.KEY_LEFT:
+            return "LEFT"
+        
     def draw_screen(self, snake, fruit, score):
         raise NotImplementedError("Subclass must implement this method")
 
     def get_game_over_event(self):
-        raise NotImplementedError("Subclass must implement this method")
+        event = self.__stdscr.getch()
+        if (event != curses.KEY_UP and
+            event != curses.KEY_UP and
+            event != curses.KEY_UP and
+            event != curses.KEY_UP and
+            event != -1): return "RESTART"
 
     def draw_game_over(self, score):
         raise NotImplementedError("Subclass must implement this method")
